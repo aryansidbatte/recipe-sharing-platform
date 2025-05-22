5903a0238d0bad351266c20768a2f6330e5a7886
linlin <jennielin656@gmail.com>
2025-05-22 06:16:31 +0000

    created databases, basic user auth, ingredients + recipes submission form, logout

---
+++ apps/recipe/controllers.py
@@ -0,0 +1,50 @@
+
+from yatl.helpers import A
+
+from py4web import URL, abort, action, redirect, request, Field
+from py4web.core import Template
+from py4web.utils.form import Form
+
+from .common import (
+    T,
+    auth,
+    authenticated,
+    cache,
+    db,
+    flash,
+    logger,
+    session,
+    unauthenticated,
+)
+
+import re
+
+from py4web import action, request, abort, redirect
+from .common import auth
+
+
+@action("index")
+@action.uses("index.html", auth.user, T)
+def index():
+    user = auth.get_user()
+    ingredients_form = Form(db.ingredients)
+    # Recipe form WITHOUT author field (hidden)
+    recipes_form = Form(
+        db.recipes,
+        fields=["name", "type", "description", "image", "instruction_steps", "servings"],
+        dbio=False #disables automatic inserts
+    )
+    
+    if recipes_form.accepted:
+        print("Inserting recipe:", recipes_form.vars["name"])
+        db.recipes.insert(
+            name=recipes_form.vars["name"],
+            type=recipes_form.vars["type"],
+            description=recipes_form.vars["description"],
+            image=recipes_form.vars["image"],
+            instruction_steps=recipes_form.vars["instruction_steps"],
+            servings=recipes_form.vars["servings"],
+            author=user["id"]
+        )
+        redirect(URL("index"))
+    return {"user": user, "ingredients_form": ingredients_form, "recipes_form": recipes_form}
---
+++ apps/recipe/models.py
@@ -0,0 +1,34 @@
+from pydal.validators import *
+from .common import Field, db, auth
+
+db.define_table(
+    "ingredients",
+    Field("name", type="string", requires=IS_NOT_EMPTY()),
+    Field("unit", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
+    Field("calories_per_unit", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
+    Field("description", type="string", requires=IS_NOT_EMPTY()),
+)
+
+db.define_table(
+    "recipes",
+    Field("name", type="string", requires=IS_NOT_EMPTY()),
+    Field("type", type="string", requires=IS_NOT_EMPTY()),
+    Field("description", type="string", requires=IS_NOT_EMPTY()),
+    Field("image", type="upload"),
+    Field("instruction_steps",type="string", requires=IS_NOT_EMPTY()),
+    Field("servings", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
+    Field("author", "reference auth_user", requires=IS_NOT_EMPTY(), readable=False, writable=False),
+)
+
+db.define_table(
+    "link",
+    Field("recipe_id", "reference recipes", requires=IS_NOT_EMPTY()),
+    Field("ingredient_id", "reference ingredients", requires=IS_NOT_EMPTY()),
+    Field("quantity_per_serving", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
+)
+
+db.commit()
+
+# for table in db.tables:
+#     db[table].drop()
+# db.commit()
